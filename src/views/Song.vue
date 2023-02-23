2aab31fd40d856b8339b79001698e97d428e6d2f
Aladdin Sonni <aladdin.sonni@gmail.com>
2023-02-23 02:09:50 +0200

    Section 14: Finish 185. Path Parameters Lecture

---
+++ src/views/Song.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>{{ songId }}</div>
+</template>
+
+<script>
+export default {
+  name: 'Song',
+  props: {
+    songId: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>
